110d569895e16a57dfc5e6860610e666ef6881bd
gitmahin <nimahin25@gmail.com>
2026-09-06 21:21:52 +0600

    chore: remove the folder that was created by mistake

--- .react-email/src/app/preview/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsPanelProvider } from '../../contexts/props-panel';
-
-export default function PreviewLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <PropsPanelProvider>{children}</PropsPanelProvider>;
-}
