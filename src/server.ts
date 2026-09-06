b507e9e4278917483a2e752c985d26be3c677669
gitmahin <nimahin25@gmail.com>
2026-09-06 17:14:42 +0600

    chore: remove comments

--- src/server.ts
+++ src/server.ts
@@ -39,11 +39,11 @@ if (process.env.NODE_ENV === "development") {
 /* -------------------------------------------------------------------------- */
 await connectRedis();
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 15 * 60 * 1000,
   limit: baseConfig.NODE_ENV == "test" ? 5000 : 100,
-  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-  ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+  standardHeaders: "draft-8",
+  legacyHeaders: false,
+  ipv6Subnet: 56,
   passOnStoreError: false,
   store: new RedisStore({
     sendCommand: (...args: string[]) => redisClient.sendCommand(args),
